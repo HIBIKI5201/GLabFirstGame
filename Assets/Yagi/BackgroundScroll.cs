c8f91f371c404604d8cd90297df47d17fa897609
KRYG <abcdef0509a@gmail.com>
2024-09-15 00:30:08 +0900

    背景スクロールの修正

--- Assets/Yagi/BackgroundScroll.cs
+++ Assets/Yagi/BackgroundScroll.cs
@@ -2,26 +2,31 @@ using UnityEditor.U2D.Animation;
 using UnityEngine;
 public class BackgroundScroll : MonoBehaviour
 {
-    float _length;
-    float _startpos;
+    [Tooltip("背景のX軸の大きさ")] float _length;
+    [Tooltip("最初の座標")]float _startpos;
     /// <summary> スクロールさせる速さ</summary>
+    [Header("スクロールの速さ")]
     [SerializeField] float _scrollSpeed;
     [SerializeField, Tooltip("スクロールさせる背景素材")] GameObject _background;
     SpriteRenderer _backgroundSprite;
-
-    /// <summary>背景の複製を保存する</summary>
+    /// <summary>背景の複製</summary>
     SpriteRenderer _backgroundClone1;
+    /// <summary>背景の複製</summary>
     SpriteRenderer _backgroundClone2;
 
     void Start()
     {
+        //スピードの+-を合わせる
+        _scrollSpeed *= -1;
+
         // 背景画像のx座標
-        _startpos = transform.position.x;
+        _startpos = _background.transform.position.x;
 
         _backgroundSprite = _background.GetComponent<SpriteRenderer>();
         // 背景画像のx軸方向の幅
         _length = _backgroundSprite.GetComponent<SpriteRenderer>().bounds.size.x;
 
+        //背景を左右に複製して背景オブジェクトの子オブジェクトにする
         _backgroundClone1 = Instantiate(_backgroundSprite);
         _backgroundClone1.transform.Translate(-_length, 0, 0);
         _backgroundClone1.transform.SetParent(_backgroundSprite.transform);
