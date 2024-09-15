0f444e7e52243bc0174422c72e7a565bfe8760b6
KasaharaSota <souta.programer1201@gmail.com>
2024-09-15 16:33:28 +0900

    Playerのジャンプの仕様の改善(まだ調整中)

--- Assets/Kasahara/PlayerController.cs
+++ Assets/Kasahara/PlayerController.cs
@@ -1,6 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditorInternal.Profiling.Memory.Experimental;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
@@ -8,8 +7,8 @@ public class PlayerController : MonoBehaviour
 {
     /// <summary>プレイヤーキャラのHP。これが0になるとゲームオーバー</summary>
     [Header("体力の最大値")]
-    [SerializeField][Tooltip("プレイヤーの体力の最大値")] int _maxHP;
-    int _currentHP;
+    [SerializeField][Tooltip("プレイヤーの体力の最大値")] int _maxHp;
+    int _currentHp;
     /// <summary>プレイヤーキャラが攻撃を行った時に、エネミーに与えるダメージ。</summary>
     [Header("攻撃力")]
     [SerializeField][Tooltip("プレイヤーの攻撃力")] int _attack;
@@ -19,41 +18,48 @@ public class PlayerController : MonoBehaviour
     /// <summary>プレイヤーキャラクターの移動速度の加速度を決める値。数値が高いほど最大速度までの加速時間が短い</summary>
     [Header("移動速度の加速度")]
     [SerializeField][Tooltip("プレイヤーの移動速度の加速度")] float _movePower;
-    /// <summary>プレイヤーキャラクターのJump時に上方向に掛ける力。数値が高いほど、高くジャンプが行える</summary>
+    /// <summary>プレイヤーキャラクターのJump時に上方向に掛ける力。</summary>
     [Header("ジャンプ力")]
     [SerializeField][Tooltip("プレイヤーのジャンプ力")] float _jumpPower;
     /// <summary>エネミーからダメージを受けた際、一定時間はダメージを受けないようにする時間。整数値で入力する。1=1秒</summary>
     [Header("無敵時間")]
     [SerializeField][Tooltip("プレイヤーの無敵時間")] int _damageCool;
     /// <summary>接地判定</summary>
-   　bool _isGround;
+   　bool _isJump;
+    /// <summary>敵を踏んだ判定</summary>
+    bool _isStompEnemy;
     /// <summary>持っているアイテムのリスト</summary>
     List<ItemBase> _itemList = new List<ItemBase>();
     Rigidbody2D _rb;
-
+    float _jumpTimer = 0;
     void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
+        _currentHp = _maxHp;
     }
 
     void Update()
     {
-        var x = Input.GetAxisRaw("Horizontal");
-        Move(new Vector2(x, 0));
+        Move();
         Jump();
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Ground"))
         {
-            _isGround = true;
+            _isJump = false;
+            _jumpTimer = 0;
+        }
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            _isStompEnemy = true;
         }
     }
-    private void Move(Vector2 horiMove)
+    private void Move()
     {
         //移動処理
         var x = Input.GetAxisRaw("Horizontal");
-        _rb.AddForce(horiMove * _movePower, ForceMode2D.Force);
+        _rb.AddForce(new Vector2(x, 0) * _movePower, ForceMode2D.Force);
         if (_rb.velocity.x > _speed)
         {
             _rb.velocity = new Vector2(_speed, _rb.velocity.y);
@@ -65,11 +71,29 @@ public class PlayerController : MonoBehaviour
     }
     private void Jump()
     {
-        if (_isGround && Input.GetKeyDown(KeyCode.Space))
+        if (!_isJump && Input.GetKeyDown(KeyCode.Space))
         {
             _rb.AddForce(new Vector2(0, _jumpPower), ForceMode2D.Impulse);
-            _isGround = false;
+            _isJump = true;
         }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            if (_isStompEnemy)
+            {
+                _isStompEnemy = false;
+                _rb.AddForce(new Vector2(0, _jumpPower), ForceMode2D.Impulse);
+            }
+        }
+        else if (_isStompEnemy)
+        {
+            _rb.AddForce(new Vector2(0, _jumpPower / 1.5f), ForceMode2D.Impulse);
+            _isStompEnemy = false;
+        }
+        else if (_rb.velocity.y > 0)
+        {
+            _rb.velocity = new Vector2(_rb.velocity.x, _rb.velocity.y * 0.99f);
+        }
+        //Debug.Log(_isJump);
     }
     public void GetItem(ItemBase item)
     {
@@ -81,10 +105,11 @@ public class PlayerController : MonoBehaviour
     /// <param name="value"></param>
     public void FluctuationLife(int value)
     {
-        _currentHP += value;
-        if(_currentHP > _maxHP)
+        _currentHp += value;
+        if (_currentHp > _maxHp)
         {
-            _currentHP = _maxHP;
+            _currentHp = _maxHp;
         }
+        Debug.Log($"Playerの体力:{_currentHp}");
     }
 }
