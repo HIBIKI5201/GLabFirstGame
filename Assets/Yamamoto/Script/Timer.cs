33e9f763f856d6fe3c386f993e7af00f9374ee64
Royzn <yamiryo22@gmail.com>
2024-09-15 16:14:46 +0900

    時間制限(必要だったらタイムアップ時の処理も追記可能)

---
+++ Assets/Yamamoto/Script/Timer.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] Text timerTxt;
+    [SerializeField][Header("ŠJnŠÔ")] float _startTime;
+    float _currentTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _currentTime = _startTime;
+        UpdateTimerText();
+    }
+
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if(_currentTime > 0)
+        {
+            _currentTime -= Time.deltaTime;
+            UpdateTimerText();
+        }
+        else
+        {
+            _currentTime = 0;
+            UpdateTimerText();
+            //HP0‚Ì‚Ìˆ—
+        }
+    }
+
+    void UpdateTimerText()
+    {
+        int min = Mathf.FloorToInt(_currentTime / 60);
+        int sec = Mathf.FloorToInt(_currentTime % 60);
+        timerTxt.text = string.Format("{0:00}:{1:00}", min, sec);
+    }
+}
