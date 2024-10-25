a22b62a818e80a435f8fcc6804518f8d78e976bd
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2024-10-26 06:53:37 +0900

    ステージ3のクリア演出調整

--- Assets/Planner/Script/Stage3GoalPerformance.cs
+++ Assets/Planner/Script/Stage3GoalPerformance.cs
@@ -9,13 +9,13 @@ using UnityEngine.UI;
 /// </summary>
 public class Stage3GoalPerformance : MonoBehaviour
 {
-    [SerializeField] CanvasGroup _clearPanel;
+    [SerializeField] CanvasGroup _clearPanel, _uiSet;
     [SerializeField] Image _vignette;
     [SerializeField] CinemachineVirtualCamera _cam;
     [SerializeField] GameObject[] _fukidasi;
     [SerializeField] Text _text;
     [SerializeField] Goal _goal;
-
+    [SerializeField] RedImage _redImage;
 
     void Start()
     {
@@ -45,12 +45,15 @@ public class Stage3GoalPerformance : MonoBehaviour
         yield return new WaitForSeconds(panelTime);
 
         _clearPanel.DOFade(0, 1);
+        _uiSet.DOFade(0, 1);
+        AudioManager.Instance._bgmSource.DOFade(0,4);
 
         yield return new WaitForSeconds(warkTime - panelTime);
 
         //立ち止まってセリフが開始
 
         _fukidasi[0].SetActive(true);
+        _text.text = "";
         _text.gameObject.SetActive(true);
         _text.DOText("おばあさん。赤ずきんだよ。",2f);
 
@@ -78,6 +81,8 @@ public class Stage3GoalPerformance : MonoBehaviour
             _text.text = "";
             _text.color = Color.red;
             _text.DOText("食べたりしないからさあ、開けてよお？", 4f);
+
+            yield return new WaitForSeconds(4f);
         }
         else //HappyEnd
         {
@@ -92,7 +97,7 @@ public class Stage3GoalPerformance : MonoBehaviour
 
         yield return new WaitForSeconds(3f);
 
-        _goal.StartCoroutine(_goal.LoadScene(4f));
+        _goal.StartCoroutine(_goal.LoadScene(1f));
 
     }
 }
