72e9246b56c1bbd254b614f866a99d125d53ddb0
KRYG <abcdef0509a@gmail.com>
2024-10-26 15:23:07 +0900

    [update]チェックポイント修正

--- Assets/Yagi/Sqript/Checkpoint.cs
+++ Assets/Yagi/Sqript/Checkpoint.cs
@@ -13,14 +13,26 @@ public class Checkpoint : MonoBehaviour
 
     CapsuleCollider2D _capsuleCollider;
 
+    static Vector2 _startPlayerPos;
+
     public Vector2 _checkpoint { get; set; }
 
     public bool _isCheck = false;
 
     private void Start()
     {
+        if (_startPlayerPos == Vector2.zero)_startPlayerPos = _player.transform.position;
         _capsuleCollider = GetComponent<CapsuleCollider2D>();
         _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        if (_gameManager.State == GameManager.GameState.StageClear)
+        {
+            Debug.Log("チェックポイントをリセット");
+            _checkpoint = _startPlayerPos;
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
