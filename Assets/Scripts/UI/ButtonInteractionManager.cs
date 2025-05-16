ae21c3efda1a3998653cafe3b3d6e1d4fae3ea25
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-05-17 00:58:27 +0900

    [fix] リファクタリング

---
+++ Assets/Scripts/UI/ButtonInteractionManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// ボタン操作後の挙動を制御するコンポーネント
+/// タイトルシーンやステージ選択シーンでのボタン操作後、ボタンの無効化や効果音の設定を管理する
+/// </summary>
+public class ButtonInteractionManager : MonoBehaviour
+{
+    [SerializeField] private SelectedButton[] _selectedButtons;
+    
+    /// <summary>
+    /// ボタン操作後の処理を実行する
+    /// </summary>
+    public void Mute()
+    {
+        Invoke(nameof(VolumeChange), 0.3f);
+        
+        // 全てのボタンを押せないようにする
+        foreach (var button in _selectedButtons)
+        {
+            button.enabled = false;
+        }
+    }
+
+    /// <summary>
+    /// SE用のAudioSourceの音量をゼロにして絶対にSEが鳴らないようにする
+    /// </summary>
+    private void VolumeChange()
+    {
+        AudioManager.Instance.SESource.volume = 0;
+    }
+}
