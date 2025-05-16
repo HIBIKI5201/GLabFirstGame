3f93ee3d090f4c40102d800d12bcd73548508b74
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-05-17 01:20:59 +0900

    [fix] フェードイン処理にコメントを追加

--- Assets/Scripts/UI/FadeIn.cs
+++ Assets/Scripts/UI/FadeIn.cs
@@ -2,19 +2,17 @@ using UnityEngine;
 using UnityEngine.UI;
 using DG.Tweening;
 
+/// <summary>
+/// フェードイン処理
+/// </summary>
 public class FadeIn : MonoBehaviour
 {
-    public float _fadeTime = 2f;
-    Image _image;
-    void Start()
+    [SerializeField] private float _fadeTime = 2f; // フェードにかける時間
+    private Image _image;
+    
+    private void Start()
     {
         _image = GetComponent<Image>();
-        _image.DOFade(0,_fadeTime);
-        Destroy(this.gameObject,_fadeTime);
-    }
-
-    private void Delete()
-    {
-        Destroy(this.gameObject);
+        _image.DOFade(0,_fadeTime).OnComplete(() => Destroy(this)); // フェードアニメーションのあと自身を破棄する
     }
 }
