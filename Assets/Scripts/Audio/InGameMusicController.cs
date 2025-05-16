94fca6f6423718b4e74da522cd8fcc36fe394edf
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-05-16 22:50:44 +0900

    [fix] InGameのBGM再生クラスも他のクラスに合わせて命名を変更

---
+++ Assets/Scripts/Audio/InGameMusicController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// インゲームシーンで各ステージのBGMを再生するクラス
+/// </summary>
+public class InGameMusicController : MonoBehaviour
+{
+    [SerializeField] private StageType _stageType;
+
+    private void Start()
+    {
+        int index = _stageType switch // switch式の書き方
+        {
+            StageType.Stage1 => 2,
+            StageType.Stage2 => 3,
+            StageType.Stage3 => 5,
+            _ => 2
+        };
+        AudioManager.Instance.PlayBGM(AudioManager.Instance._bgmSounds[index]._name);
+    }
+}
---
+++ Assets/Scripts/Enum/StageType.cs
@@ -0,0 +1,9 @@
+/// <summary>
+/// ステージの種類の列挙型
+/// </summary>
+public enum StageType
+{
+    Stage1,
+    Stage2,
+    Stage3,
+}
