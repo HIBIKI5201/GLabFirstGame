732d012e300b1d7f4a255499ef3db3ad5e5abbc2
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-05-16 22:20:59 +0900

    [update] Sceneの列挙型をSceneLoaderクラスから分離

---
+++ Assets/Scripts/Enum/SceneType.cs
@@ -0,0 +1,14 @@
+/// <summary>
+/// シーン名の列挙型
+/// </summary>
+public enum SceneType
+{
+    None = -1,
+    Title,
+    InGame,
+    Ending,
+    SelectStage,
+    Stage1,
+    Stage2,
+    Stage3,
+}
