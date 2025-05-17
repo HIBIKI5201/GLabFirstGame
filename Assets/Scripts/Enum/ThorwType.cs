b79626ec13455871cb747223e5eda54b651508d8
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-05-17 11:39:01 +0900

    [update] ドキュメントコメントを追加

--- Assets/Scripts/Enum/ThorwType.cs
+++ Assets/Scripts/Enum/ThorwType.cs
@@ -3,6 +3,13 @@
 /// </summary>
 public enum ThrowType
 {
+    /// <summary>
+    /// 一直線に飛ぶ
+    /// </summary>
     Straight,
+    
+    /// <summary>
+    /// 放物線を描いて飛ぶ
+    /// </summary>
     Parabola
 }
