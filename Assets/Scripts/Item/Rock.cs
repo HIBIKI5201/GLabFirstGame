f2e94edb1d98f611545106bd33f162e315c36d5e
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-05-17 11:37:53 +0900

    [fix] 石クラスも修正

--- Assets/Scripts/Item/Rock.cs
+++ Assets/Scripts/Item/Rock.cs
@@ -1,31 +1,38 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// アイテム：石
+/// </summary>
 public class Rock : ItemBase
 {
+    protected override void Activate()
+    {
+        // 投げていない時であれば、以降の処理は行わない
+        if (!IsThrowing) return;
+        
+        var hit = Physics2D.OverlapCircleAll(transform.position, EffectRange);
+        foreach (var obj in hit)
+        {
+            if (obj.CompareTag("Enemy"))
+            {
+                if (obj.TryGetComponent<Enemy>(out var enemy))
+                {
+                    enemy.ReactionStone(ActiveTime);
+                    if (enemy.State != EnemyStateType.Faint)
+                    {
+                        // まだ敵が気絶状態ではなければ、SEを再生する
+                        AudioManager.Instance.PlaySE("damage_enemy");
+                    }
+                }
+                
+                Destroy(gameObject, 0.3f);
+            }
+        }
+    }
+    
     private void OnDrawGizmosSelected()
     {
         Gizmos.color = new Color(1, 1, 1, 0.5f);
         Gizmos.DrawWireSphere(transform.position, EffectRange);
-    }
-
-    protected override void Activate()
-    {
-        if (IsThrowing)
-        {
-            var hit = Physics2D.OverlapCircleAll(transform.position, EffectRange);
-            foreach (var obj in hit)
-            {
-                if (obj.gameObject.CompareTag("Enemy"))
-                {
-                    //�G���X�^�������鏈��
-                    obj.gameObject.GetComponent<Enemy>().ReactionStone(ActiveTime);
-                    if(obj.gameObject.GetComponent<Enemy>().State != EnemyStateType.Faint)
-                    AudioManager.Instance.PlaySE("damage_enemy");
-                    Destroy(gameObject,0.3f);
-                }
-            }
-        }
     }
 }
