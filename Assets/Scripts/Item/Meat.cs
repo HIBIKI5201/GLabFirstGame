9fc87c05c85245167038e727eb187d8709061c8f
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-05-17 11:34:08 +0900

    [update] ボトルクラスに合わせてお肉も変更

--- Assets/Scripts/Item/Meat.cs
+++ Assets/Scripts/Item/Meat.cs
@@ -1,63 +1,88 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// アイテム：肉
+/// </summary>
 public class Meat : ItemBase
 {
-    [SerializeField, Header("�ڒn����̑傫��")] Vector2 _size;
-    [SerializeField, Header("�ڒn����̊p�x")] float _angle;
-    bool _effected;
-    Rigidbody2D _rb;
+    [SerializeField] private Vector2 _size;
+    [SerializeField] private float _angle;
+    private bool _effected;
+    private Rigidbody2D _rb;
+
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    protected override void Activate()
+    {
+        // 投げていない時であれば、以降の処理は行わない
+        if (!IsThrowing) return;
+
+        if (!Landing)
+        {
+            CheckForLanding();
+        }
+        else
+        {
+            HandleLanded();
+        }
+    }
+
+    /// <summary>
+    /// 地面に衝突したかの判定
+    /// </summary>
+    private void CheckForLanding()
+    {
+        var hit = Physics2D.OverlapBoxAll(transform.position, _size, _angle);
+        foreach (var obj in hit)
+        {
+            if (obj.gameObject.CompareTag("Ground"))
+            {
+                Landing = true;
+                GetComponent<Collider2D>().enabled = true;
+                AudioManager.Instance.PlaySE("meat");
+            }
+        }
+    }
+
+    /// <summary>
+    /// 地面に落ちたあとの処理
+    /// </summary>
+    private void HandleLanded()
+    {
+        if (!_effected)
+        {
+            var hit = Physics2D.OverlapCircleAll(transform.position, EffectRange);
+            
+            
+            foreach (var obj in hit)
+            {
+                if (obj.gameObject.CompareTag("Enemy"))
+                {
+                    // 敵のリアクションを呼び出す
+                    obj.gameObject.GetComponent<Enemy>().ReactionMeat(transform.position, ActiveTime);
+                }
+            }
+
+            _effected = true;
+            Destroy(gameObject, ActiveTime);
+        }
+
+        if (_effected && _rb.velocity.y == 0)
+        {
+            _rb.velocity = Vector2.zero;
+            _rb.angularVelocity = 0;
+            _rb.bodyType = RigidbodyType2D.Kinematic;
+            gameObject.GetComponent<Collider2D>().enabled = false;
+        }
+    }
+
     private void OnDrawGizmosSelected()
     {
         Gizmos.color = new Color(1, 1, 1, 0.5f);
         Gizmos.DrawWireSphere(transform.position, EffectRange);
         Gizmos.DrawWireCube(transform.position, _size);
-    }
-
-    protected override void Activate()
-    {
-        if (IsThrowing)
-        {
-            if (!Landing)
-            {
-                var hit = Physics2D.OverlapBoxAll(transform.position, _size, _angle);
-                foreach (var obj in hit)
-                {
-                    if (obj.gameObject.CompareTag("Ground"))
-                    {
-                        Landing = true;
-                        //�n�ʂɂ�����R���C�_�[�𕜊�
-                        GetComponent<Collider2D>().enabled = true;
-                        AudioManager.Instance.PlaySE("meat");
-                    }
-                }
-            }
-            else
-            {
-                if (!_effected)
-                {
-                    var hit = Physics2D.OverlapCircleAll(transform.position, EffectRange);
-                    _rb = GetComponent<Rigidbody2D>();
-                    foreach (var obj in hit)
-                    {
-                        if (obj.gameObject.CompareTag("Enemy"))
-                        {
-                            //�G��U�����鏈��
-                            obj.gameObject.GetComponent<Enemy>().ReactionMeat(transform.position, ActiveTime);
-                        }
-                    }
-                    Destroy(gameObject, ActiveTime);
-                    _effected = true;
-                }
-                if (_effected && _rb.velocity.y == 0)
-                {
-                    _rb.velocity = Vector2.zero;
-                    _rb.angularVelocity = 0;
-                    _rb.bodyType = RigidbodyType2D.Kinematic;
-                    gameObject.GetComponent<Collider2D>().enabled = false;
-                }
-            }
-        }
     }
 }
