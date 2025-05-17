bd80e25267d47ff6784715b0db35e0e410a5c01d
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-05-17 09:07:22 +0900

    [fix] 敵の種類の列挙型を分離

---
+++ Assets/Scripts/Enum/EnemyType.cs
@@ -0,0 +1,30 @@
+/// <summary>
+/// 敵の種類の列挙型
+/// </summary>
+public enum EnemyType
+{
+    /// <summary>
+    /// 野犬
+    /// </summary>
+    StrayDog,
+
+    /// <summary>
+    /// 狼_普通
+    /// </summary>
+    Wolf_Normal,
+
+    /// <summary>
+    /// 狼_灰
+    /// </summary>
+    Wolf_Gray,
+
+    /// <summary>
+    /// 熊
+    /// </summary>
+    Bear,
+
+    /// <summary>
+    /// ボス狼
+    /// </summary>
+    Boss_Wolf
+}
