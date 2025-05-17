ede087908f89f4112eed9285e728d031dcfdb307
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-05-17 09:07:35 +0900

    [fix] 敵のステートの列挙型も分離

---
+++ Assets/Scripts/Enum/EnemyStateType.cs
@@ -0,0 +1,30 @@
+/// <summary>
+/// 敵の状態の列挙型
+/// </summary>
+public enum EnemyStateType
+{
+    /// <summary>
+    /// 通常
+    /// </summary>
+    Normal,
+
+    /// <summary>
+    /// 気絶中（アイテム：石の効果）
+    /// </summary>
+    Faint,
+
+    /// <summary>
+    /// 食いついている（アイテム：肉の効果）
+    /// </summary>
+    Bite,
+
+    /// <summary>
+    /// 逃げている（アイテム：空き瓶の効果）
+    /// </summary>
+    Escape,
+
+    /// <summary>
+    /// 追跡中
+    /// </summary>
+    Chase,
+}
