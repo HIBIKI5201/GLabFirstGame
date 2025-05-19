7ae3ce84e69d435ed81c383d72a3bf781858f72c
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-05-19 15:39:20 +0900

    [clean] チェックポイントオブジェクトのクラスをきれいに

--- Assets/Scripts/StageSystem/Checkpoint.cs
+++ Assets/Scripts/StageSystem/Checkpoint.cs
@@ -1,21 +1,17 @@
 using UnityEngine;
 
-
+/// <summary>
+/// チェックポイントオブジェクトを管理するクラス
+/// </summary>
 public class Checkpoint : MonoBehaviour
 {
-    GameManager _gameManager;
-
-    [SerializeField] GameObject _player;
-
-    [Header("変更するイラスト"), SerializeField] Sprite _changeSprite;
-
-    SpriteRenderer _spriteRenderer;
-
-    CapsuleCollider2D _capsuleCollider;
-
-    [SerializeField] int nowStage;
-
-    private bool _isFirstCheck = false;
+    [SerializeField] private GameObject _player;
+    [Header("チェックポイント通過後のイラスト"), SerializeField] private Sprite _changeSprite;
+    [SerializeField] private int nowStage;
+    private GameManager _gameManager;   
+    private SpriteRenderer _spriteRenderer;
+    private CapsuleCollider2D _capsuleCollider;
+    private bool _isFirstCheck = false; // 通過済みかどうか
 
     private void Start()
     {        
@@ -30,22 +26,22 @@ public class Checkpoint : MonoBehaviour
             ResetPoint();
         }
     }
-
-   
-    public void ResetPoint()
-    {
-        CheckPointManager._checkPoint[nowStage - 1] = Vector2.zero;
-    }
+    
+    /// <summary>
+    /// 現在のステージのリスポーン地点をVector2.zeroの位置にセットしなおす
+    /// </summary>
+    public void ResetPoint() => CheckPointManager._checkPoint[nowStage - 1] = Vector2.zero;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        // Colliderにプレイヤーが入って、かつまだチェックポイント通過前だった場合
         if (collision.gameObject.CompareTag("Player") && !_isFirstCheck)
         {
             AudioManager.Instance.PlaySE("checkpoint");
-            CheckPointManager._checkPoint[nowStage - 1] = this.transform.position;
-            _capsuleCollider.enabled = false;
-            if (_changeSprite) _spriteRenderer.sprite = _changeSprite;
-            _isFirstCheck = true;
+            CheckPointManager._checkPoint[nowStage - 1] = transform.position; // リスポーン地点を変更する
+            _capsuleCollider.enabled = false; // コライダーを無効化
+            if (_changeSprite) _spriteRenderer.sprite = _changeSprite; // 画像を変更
+            _isFirstCheck = true; // 通過済みとする
         }
     }
 }
