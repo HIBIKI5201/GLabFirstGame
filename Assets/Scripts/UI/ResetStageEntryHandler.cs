cb342739ec1f61c09c5775ed0023f869b40b3bfc
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-05-20 07:38:53 +0900

    [fix] 命名変更

---
+++ Assets/Scripts/UI/ResetStageEntryHandler.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+/// <summary>
+/// ステージ選択をしたタイミングで、ステージに初めて入るフラグをリセットする
+/// </summary>
+public class StageTitleActive : MonoBehaviour
+{
+    /// <summary>
+    /// ステージが選択された際に呼び出され、初回進入フラグをリセットする
+    /// </summary>
+    public void ResetStageEntryState()
+    {
+        InGameStartSequence._isFirstEntry = true;
+    }
+}
