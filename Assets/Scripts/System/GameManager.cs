d76253a073f6e3a00421e098e1038a53a3df98a4
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-05-21 21:32:15 +0900

    [fix] Stateをリアクティブプロパティに

--- Assets/Scripts/System/GameManager.cs
+++ Assets/Scripts/System/GameManager.cs
@@ -1,28 +1,21 @@
+using UniRx;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
     
-    public GameStateType stateType;
-    public GameStateType StateType 
-    { 
-        get => stateType; 
-        set 
-        {
-            if (stateType == value)
-            {
-                return; // 既にその状態だったら代入処理は行わない
-            }
-            
-            stateType = value;
-        } 
-    }
-    
     [Header("フレームレートの設定")]
     [SerializeField] private bool _settingFPS;
     [SerializeField,Range(10,120)] private int _targetFPS;
 
+    /// <summary>
+    /// 現在のゲームの状態を表すリアクティブプロパティ
+    /// </summary>
+    public ReactiveProperty<GameStateType> CurrentStateProp => _currentStateProp;
+    private readonly ReactiveProperty<GameStateType> _currentStateProp = new ReactiveProperty<GameStateType>();
+    public GameStateType CurrentState => CurrentStateProp.Value;
+    
     private void Awake()
     {
         Instance = this;
@@ -33,4 +26,9 @@ public class GameManager : MonoBehaviour
         // FPS設定を行うか。行わない場合、フレームレートの制限を行わず、デバイスが出せる最大のフレームレートで動作する
         Application.targetFrameRate = _settingFPS ? _targetFPS : -1; 
     }
+    
+    /// <summary>
+    /// ステートを変更する
+    /// </summary>
+    public void ChangeState(GameStateType newState) => CurrentStateProp.Value = newState;
 }
