d4bbac04e090cd2451dff1e13e4688103e85e671
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-05-22 20:43:06 +0900

    [fix] 文字化けを削除

--- Assets/Scripts/Data/GroundedRay.cs
+++ Assets/Scripts/Data/GroundedRay.cs
@@ -4,27 +4,12 @@ using UnityEngine;
 [Serializable]
 public struct GroundedRay
 {
-    [Tooltip("Ray����������LayerMask")]
     public LayerMask _mask;
-
-    [Tooltip("����Ray����������LayerMask")]
     public LayerMask _sideMask;
-
-    [Tooltip("�ǂ�G�A�v���C���[�𔻒f����")]
     public Vector2 _sideRayPos;
-
-    [Tooltip("�E���̊R�𔻒f����Ray�̒��S")]
     public Vector2 _rightRayPos;
-
-    [Tooltip("�����̊R�𔻒f����Ray�̒��S")]
     public Vector2 _leftRayPos;
-
-    [Space, Tooltip("�R�𔻒f����Ray�̒���")]
     public float _rayLong;
-
-    [Tooltip("�ǂ𔻒f����Ray�̒���")]
     public float _sideRayLong;
-
-    [Space, Tooltip("��Ԃ̂𔻒f����Ray�̒���")]
     public float _jumpRayLong;
 }
