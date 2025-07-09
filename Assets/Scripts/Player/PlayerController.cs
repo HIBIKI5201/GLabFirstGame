55c2edbf8a0813b07127443543768746497baa5c
masa101224 <masa101224@outlook.jp>
2025-07-09 22:28:25 +0900

    エラー修正

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,9 +10,15 @@ using UnityEngine.UI;
 /// <summary>
 /// Playerの動きを管理するクラス
 /// </summary>
-[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Rigidbody2D), typeof(AudioSource))]
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField, ReadOnly]
+    private Rigidbody2D _rigidbody2D;
+
+    [SerializeField, ReadOnly]
+    private AudioSource _audioSource;
+
     [SerializeField] int _maxHp;
     [SerializeField] int _initHp;
     public int CurrentPetal;
@@ -42,37 +48,44 @@ public class PlayerController : MonoBehaviour
     bool _isInvincible;
     [SerializeField] bool _canAction = true;
     [HideInInspector] public PlayerStatusType _playerStatus = PlayerStatusType.Normal;
-    Rigidbody2D _rb;
-    SpriteRenderer _spriteRenderer;
+    [SerializeField] private EnemyGetter _enemyGetter;
     Scene m_simulationScene;
     PhysicsScene2D m_physicsScene;
-    GameObject[] _itemPos = new GameObject[3];
     float _horiInput = 0;
     CameraShakeController _cameraShakeController;
     DamageEffect _damageEffect;
     HealingEffect _healingEffect;
     PauseManager _pauseManager;
-    AudioSource _audioSource;
     Animator _animator;
     Animator _petalGuageAnimator;
-    Vector2 _pauseVelocity;
     float _veloX = 0;
     float _acce = 1;
     IEnumerator _jumpEnumerator;
-    
+    private Collider2D _playerCollider;
+
+    public static bool IsPlayerInGrass
+    {
+        get;
+        private set;
+    }
+
+    private void OnValidate()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+        _audioSource = GetComponent<AudioSource>();
+    }
+
     private void Awake()
     {
-        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _playerCollider = GetComponent<Collider2D>();
         _damageEffect = GetComponent<DamageEffect>();
         _healingEffect = GetComponent<HealingEffect>();
 
-        if (!TryGetComponent(out _audioSource)) Debug.LogError("AudioSourceが設定されていません");
-        
         // PauseManager
         _pauseManager = FindAnyObjectByType<PauseManager>();
         if (_pauseManager != null) _pauseManager.OnPauseResume += PauseAction; // ポーズイベントを購読
         else Debug.LogError("PauseManagerが取得できませんでした");
-           
+
         if (!TryGetComponent(out _animator))
         {
             _animator = GetComponentInChildren<Animator>();
@@ -82,9 +95,8 @@ public class PlayerController : MonoBehaviour
     private void Start()
     {
         CurrentHp = _initHp;
-        _rb = GetComponent<Rigidbody2D>();
         CreatePhysicsScene();
-        
+
         GameObject platform;
         if (_throwsetting.Platform != null)
         {
@@ -92,15 +104,14 @@ public class PlayerController : MonoBehaviour
             Array.ForEach(platform.GetComponentsInChildren<Renderer>(), x => x.enabled = false);
             SceneManager.MoveGameObjectToScene(platform, m_simulationScene);
         }
-        
+
         _cameraShakeController = FindAnyObjectByType<CameraShakeController>();
         if (_cameraShakeController == null) Debug.LogError("CameraShakeControllerが設定されていません");
-        
+
         _itemSetting.RockUi.GetComponent<Image>().color = _itemSetting.ZeroItemColor;
         _itemSetting.BottleUi.GetComponent<Image>().color = _itemSetting.ZeroItemColor;
         _itemSetting.MeatUi.GetComponent<Image>().color = _itemSetting.ZeroItemColor;
-        _itemPos = new GameObject[] { _itemSetting.RockUi, _itemSetting.BottleUi, _itemSetting.MeatUi };
-
+        _itemSetting.IvyUi.GetComponent<Image>().color = _itemSetting.ZeroItemColor;
         _petalGuageAnimator = _petalGuage.GetComponent<Animator>();
     }
 
@@ -115,12 +126,27 @@ public class PlayerController : MonoBehaviour
         }
         _petalGuageAnimator.SetInteger("P_PetalCount", CurrentPetal);
     }
-    
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.name == "goal") _isInvincible = true; // ゴールした時
+        if (collision.gameObject.name == "goal")
+        {
+            _isInvincible = true;
+        }
+        if (collision.gameObject.CompareTag("Grass"))
+        {
+            IsPlayerInGrass = true;
+        }
     }
-    
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Grass"))
+        {
+            IsPlayerInGrass = false;
+        }
+    }
+
     private void Move()
     {
         _horiInput = Input.GetAxisRaw("Horizontal");
@@ -129,9 +155,9 @@ public class PlayerController : MonoBehaviour
             if (!_isJump)
             {
                 float x = 0;
-                if (_rb.linearVelocity.x != 0)
+                if (_rigidbody2D.linearVelocity.x != 0)
                 {
-                    x = _rb.linearVelocity.x - (_deceleration + Mathf.Abs(_rb.linearVelocity.x)) * Mathf.Sign(_rb.linearVelocity.x) * Time.deltaTime;
+                    x = _rigidbody2D.linearVelocity.x - (_deceleration + Mathf.Abs(_rigidbody2D.linearVelocity.x)) * Mathf.Sign(_rigidbody2D.linearVelocity.x) * Time.deltaTime;
                 }
                 if (Mathf.Abs(x) < 0.2)
                 {
@@ -143,13 +169,13 @@ public class PlayerController : MonoBehaviour
                     _audioSource.clip = _walk;
                     _audioSource.Play();
                 }
-                _rb.linearVelocity = new Vector2(x, _rb.linearVelocity.y);
+                _rigidbody2D.linearVelocity = new Vector2(x, _rigidbody2D.linearVelocity.y);
                 _animator.SetFloat("isWalk", Mathf.Abs(x));
             }
         }
         else
         {
-            float x = _rb.linearVelocity.x + _movePower * _horiInput * Time.deltaTime;
+            float x = _rigidbody2D.linearVelocity.x + _movePower * _horiInput * Time.deltaTime;
             if (Mathf.Abs(x) > _maxSpeed)
             {
                 x = _maxSpeed * Mathf.Sign(x);
@@ -174,7 +200,7 @@ public class PlayerController : MonoBehaviour
                     }
                 }
             }
-            _rb.linearVelocity = new Vector2(x, _rb.linearVelocity.y);
+            _rigidbody2D.linearVelocity = new Vector2(x, _rigidbody2D.linearVelocity.y);
         }
         if (_isJump)
         {
@@ -192,10 +218,10 @@ public class PlayerController : MonoBehaviour
             }
         }
     }
-    
+
     private void Jump()
     {
-        if (_rb.linearVelocity.y < -1f)
+        if (_rigidbody2D.linearVelocity.y < -1f)
         {
             if (_jumpEnumerator == null)
             {
@@ -210,7 +236,7 @@ public class PlayerController : MonoBehaviour
             if (!_isJump)
             {
                 AudioManager.Instance.PlaySE("jump");
-                _rb.AddForce(new Vector2(0, _jumpPower), ForceMode2D.Impulse);
+                _rigidbody2D.AddForce(new Vector2(0, _jumpPower), ForceMode2D.Impulse);
                 _isJump = true;
                 _jumpEnumerator = GroundingJudge(_jumpEnumerator);
                 //Debug.Log("StartCoroutine");
@@ -222,36 +248,36 @@ public class PlayerController : MonoBehaviour
             if (_isStompEnemy)
             {
                 _isStompEnemy = false;
-                _rb.linearVelocity = new Vector2(_rb.linearVelocity.x, 0);
-                _rb.AddForce(new Vector2(0, _jumpPower), ForceMode2D.Impulse);
+                _rigidbody2D.linearVelocity = new Vector2(_rigidbody2D.linearVelocity.x, 0);
+                _rigidbody2D.AddForce(new Vector2(0, _jumpPower), ForceMode2D.Impulse);
             }
         }
         else if (_isStompEnemy)
         {
-            _rb.linearVelocity = new Vector2(_rb.linearVelocity.x, 0);
-            _rb.AddForce(new Vector2(0, _jumpPower / 1.5f), ForceMode2D.Impulse);
+            _rigidbody2D.linearVelocity = new Vector2(_rigidbody2D.linearVelocity.x, 0);
+            _rigidbody2D.AddForce(new Vector2(0, _jumpPower / 1.5f), ForceMode2D.Impulse);
             _isStompEnemy = false;
         }
-        else if (_rb.linearVelocity.y > 0)
+        else if (_rigidbody2D.linearVelocity.y > 0)
         {
-            _rb.linearVelocity = new Vector2(_rb.linearVelocity.x, _rb.linearVelocity.y * 0.99f);
+            _rigidbody2D.linearVelocity = new Vector2(_rigidbody2D.linearVelocity.x, _rigidbody2D.linearVelocity.y * 0.99f);
         }
         //Debug.Log(_isJump);
     }
-    
+
     IEnumerator GroundingJudge(IEnumerator enumerator)
     {
-        if (_rb.linearVelocity.y > 0)
+        if (_rigidbody2D.linearVelocity.y > 0)
         {
             _animator.SetBool("isJump", true);
         }
         _audioSource.Stop();
-        while (_rb.linearVelocity.y > 0)
+        while (_rigidbody2D.linearVelocity.y > 0)
         {
             yield return new WaitForEndOfFrame();
         }
         _animator.SetBool("isFall", true);
-        _rb.gravityScale = _fallSpeed;
+        _rigidbody2D.gravityScale = _fallSpeed;
         while (_isJump)
         {
             yield return new WaitForEndOfFrame();
@@ -261,13 +287,13 @@ public class PlayerController : MonoBehaviour
                 if (obj.gameObject.CompareTag("Ground"))
                 {
                     _isJump = false;
-                    _rb.gravityScale = 1;
+                    _rigidbody2D.gravityScale = 1;
                     AudioManager.Instance.PlaySE("jump_landing");
                     _animator.SetBool("isJump", false);
                     _animator.SetBool("isFall", false);
                     if (_landingInertia && _horiInput == 0)
                     {
-                        _rb.linearVelocity = new Vector2(0, _rb.linearVelocity.y);
+                        _rigidbody2D.linearVelocity = new Vector2(0, _rigidbody2D.linearVelocity.y);
                     }
                     yield return new WaitForSeconds(0.5f);
                     _jumpEnumerator = null;
@@ -280,13 +306,13 @@ public class PlayerController : MonoBehaviour
                         FluctuationLife(-1);
                     }
                     _isStompEnemy = true;
-                    _rb.gravityScale = 1;
+                    _rigidbody2D.gravityScale = 1;
                 }
             }
         }
         _jumpEnumerator = null;
     }
-    
+
     /// <summary>
     /// アイテムを獲得した時の処理
     /// </summary>
@@ -331,6 +357,19 @@ public class PlayerController : MonoBehaviour
                 Destroy(item.gameObject);
             }
         }
+        else if (item as Ivy)
+        {
+            if (_itemList.Where(i => i as Ivy).ToList().Count < _itemSetting.MaxIvyCount)
+            {
+                _itemList.Add(item);
+                _itemSetting.IvyCountText.text = _itemList.Where(i => i as Ivy).Count().ToString();
+                _itemSetting.IvyUi.GetComponent<Image>().color = new Color(255, 255, 255, 255);
+            }
+            else
+            {
+                Destroy(item.gameObject);
+            }
+        }
         else if (item as Petal)
         {
             PetalGetAction();
@@ -351,6 +390,7 @@ public class PlayerController : MonoBehaviour
             }
         }
     }
+
     bool Item(out ItemBase item)
     {
         switch (_playerStatus)
@@ -364,12 +404,15 @@ public class PlayerController : MonoBehaviour
             case PlayerStatusType.Meat:
                 item = _itemList.Where(i => i as Meat).ToList().First();
                 return true;
+            case PlayerStatusType.Ivy:
+                item = _itemList.Where(i => i as Ivy).ToList().First();
+                return true;
             default:
                 item = null;
                 return false;
         }
     }
-    
+
     /// <summary>
     /// アイテムを切り替える処理
     /// </summary>
@@ -383,6 +426,7 @@ public class PlayerController : MonoBehaviour
                 _itemSetting.LeafRock.transform.localScale *= _itemSetting.LeafSize;
                 _itemSetting.LeafBottle.transform.localScale = Vector3.one;
                 _itemSetting.LeafMeat.transform.localScale = Vector3.one;
+                _itemSetting.LeafIvy.transform.localScale = Vector3.one;
             }
         }
         else if (Input.GetKeyDown(KeyCode.Alpha2))
@@ -393,6 +437,7 @@ public class PlayerController : MonoBehaviour
                 _itemSetting.LeafRock.transform.localScale = Vector3.one;
                 _itemSetting.LeafBottle.transform.localScale *= _itemSetting.LeafSize;
                 _itemSetting.LeafMeat.transform.localScale = Vector3.one;
+                _itemSetting.LeafIvy.transform.localScale = Vector3.one;
             }
         }
         else if (Input.GetKeyDown(KeyCode.Alpha3))
@@ -403,23 +448,36 @@ public class PlayerController : MonoBehaviour
                 _itemSetting.LeafRock.transform.localScale = Vector3.one;
                 _itemSetting.LeafBottle.transform.localScale = Vector3.one;
                 _itemSetting.LeafMeat.transform.localScale *= _itemSetting.LeafSize;
+                _itemSetting.LeafIvy.transform.localScale = Vector3.one;
             }
         }
         else if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            if (_itemList.Any(i => i as Ivy) && _playerStatus != PlayerStatusType.Ivy)
+            {
+                _playerStatus = PlayerStatusType.Ivy;
+                _itemSetting.LeafRock.transform.localScale = Vector3.one;
+                _itemSetting.LeafBottle.transform.localScale = Vector3.one;
+                _itemSetting.LeafMeat.transform.localScale = Vector3.one;
+                _itemSetting.LeafIvy.transform.localScale *= _itemSetting.LeafSize;
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha5))
         {
             _playerStatus = PlayerStatusType.Normal;
             _itemSetting.LeafRock.transform.localScale = Vector3.one;
             _itemSetting.LeafBottle.transform.localScale = Vector3.one;
             _itemSetting.LeafMeat.transform.localScale = Vector3.one;
+            _itemSetting.LeafIvy.transform.localScale = Vector3.one;
         }
     }
-    
+
     void CreatePhysicsScene()
     {
         m_simulationScene = SceneManager.CreateScene("Simulation", new CreateSceneParameters(LocalPhysicsMode.Physics2D));
         m_physicsScene = m_simulationScene.GetPhysicsScene2D();
     }
-    
+
     /// <summary>
     /// アイテムを投げるときの予測線のシミュレーション
     /// </summary>
@@ -464,7 +522,6 @@ public class PlayerController : MonoBehaviour
                 {
                     _damageEffect.PlayDamageEffect();
                     //_rose.RemoveAt(0);
-
                 }
                 StartCoroutine(Invincible());
                 AudioManager.Instance.PlaySE("damaged");
@@ -485,7 +542,7 @@ public class PlayerController : MonoBehaviour
             CurrentHp += value;
             _healingEffect.PlayHealingEffect();
         }
-        
+
         //if (CurrentHp > _maxHp)
         //{
         //    CurrentHp = _maxHp;
@@ -542,7 +599,7 @@ public class PlayerController : MonoBehaviour
             _pauseManager.RegisterAndStartCoroutine(ThrowItem());
         }
     }
-    
+
     IEnumerator ThrowItem()
     {
         IEnumerator enumerator = _pauseManager.GetLatestCoroutine();
@@ -560,6 +617,11 @@ public class PlayerController : MonoBehaviour
             item.transform.position = transform.position + (Vector3)_throwsetting.ThrowPos;
             rb.gravityScale = 0;
             rb.AddForce(new Vector2(_throwsetting.ThrowStraightPower * transform.localScale.x, 0), ForceMode2D.Impulse);
+        }
+        else if (item.Throw == ThrowType.Drop)
+        {
+            item.transform.position = transform.position + (Vector3)_throwsetting.ThrowPos;
+            rb.gravityScale = 1;
         }
         else
         {
@@ -611,7 +673,7 @@ public class PlayerController : MonoBehaviour
                 _itemSetting.LeafBottle.transform.localScale = Vector3.one;
             }
         }
-        else
+        else if (item as Meat)
         {
             _itemList.Remove((Meat)item);
             _itemSetting.MeatCountText.text = _itemList.Where(i => i as Meat).Count().ToString();
@@ -622,6 +684,17 @@ public class PlayerController : MonoBehaviour
                 _itemSetting.LeafMeat.transform.localScale = Vector3.one;
             }
         }
+        else if (item as Ivy)
+        {
+            _itemList.Remove((Ivy)item);
+            _itemSetting.IvyCountText.text = _itemList.Where(i => i as Ivy).Count().ToString();
+            if (_itemSetting.IvyCountText.text == "0")
+            {
+                _playerStatus = PlayerStatusType.Normal;
+                _itemSetting.IvyUi.GetComponent<Image>().color = _itemSetting.ZeroItemColor;
+                _itemSetting.LeafIvy.transform.localScale = Vector3.one;
+            }
+        }
         AudioManager.Instance.PlaySE("throw");
     EndCoroutine:
         yield return new WaitForEndOfFrame();
@@ -629,7 +702,7 @@ public class PlayerController : MonoBehaviour
     }
 
     #endregion
-    
+
     #region ポーズ関連の処理
 
     private void PauseAction(bool isPause)
@@ -643,9 +716,9 @@ public class PlayerController : MonoBehaviour
             _canAction = true;
         }
     }
-    
+
     #endregion
-    
+
     private void OnDrawGizmosSelected()
     {
         Gizmos.color = new Color(1, 1, 1, 0.5f);
@@ -672,9 +745,9 @@ public class PlayerController : MonoBehaviour
                 break;
         }
 
-        Vector2 vector2 = _rb.linearVelocity;
+        Vector2 vector2 = _rigidbody2D.linearVelocity;
         vector2.x = _veloX;
-        _rb.linearVelocity = vector2;
+        _rigidbody2D.linearVelocity = vector2;
 
         if (!_isJump)
         {
@@ -690,5 +763,5 @@ public class PlayerController : MonoBehaviour
     }
 
     #endregion
-    
+
 }
